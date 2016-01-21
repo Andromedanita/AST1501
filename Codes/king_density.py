993d3a02dbf83ccdb1a69797374f60ae09f6a527
“Anita <bahmanyar@astro.utoronto.ca>
2016-01-21 11:16:27 -0500

    problem of getting all nans solved

--- Codes/king_density.py
+++ Codes/king_density.py
@@ -8,7 +8,7 @@ sig  = 1.
 sig2 = sig**2.
 W0   = 3.
 
-rrange = np.arange(0.,100.,0.01)
+rrange = np.arange(0.01,100.,0.01)
 
 def solvr(psi, r):
 
@@ -24,11 +24,10 @@ def solvr(psi, r):
     print "efr:  ", special.erf(np.sqrt(x1)/sig)
     print "sqrt: ", np.sqrt((4. * x1)/(np.pi*sig2))
     print "last: ", 1. + ((2*x1)/(3*sig2))
+    print "2/r:", (2. * x2)/r
     print
-    
-    #print "x2' (psi''(r)): ", val_fin
 
-    return [x2, val_fin]
+    return np.array([x2, val_fin], float)
 
 
 
@@ -44,13 +43,39 @@ def dens():
     
     '''
     
-    rsol = integrate.odeint(solvr, [W0 * sig2,0.], rrange)
+    rsol = integrate.odeint(solvr, [W0 * sig2,0.01], rrange)
     w    = np.column_stack((rrange, rsol[:,0], rsol[:,1]))
     np.savetxt("test_sols.txt", w, fmt="%.9e")
     return rsol
 
-if __name__ == '__main__':
-    q = dens()
+
+#if __name__ == '__main__':
+#    q = dens()
+
+# Manual 4th-order Runge-Kutta
+a = 0.
+b = 100.
+N = 1000
+h = (b-a)/N
+
+rpoints  = np.arange(a,b,h)
+x1points = []
+x2points = []
+
+t = np.array([W0*sig2, 0.],float)
+for r in rpoints:
+    x1points.append(t[0])
+    x2points.append(t[1])
+    k1 = h*solvr(t,r)
+    k2 = h*solvr(t+0.5*k1, r+0.5*h)
+    k3 = h*solvr(t+0.5*k2, r+0.5*h)
+    k4 = h*solvr(t+k3, r+h)
+    t += (k1+2*k2+2*k3+k4)/6
+
+plt.ion()
+plt.plot(rpoints,x1points)
+plt.plot(rpoints,x2points)
+
 
 
 def psi_r(r):
