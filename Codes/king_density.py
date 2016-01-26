071f6902ee61bb2c7f5d55ad274cfb6881471302
“Anita <bahmanyar@astro.utoronto.ca>
2016-01-25 21:58:49 -0500

    divided x and y axis by r0 and rho0

--- Codes/king_density.py
+++ Codes/king_density.py
@@ -3,7 +3,7 @@ import matplotlib.pylab as     plt
 from   scipy            import integrate, special, interpolate
 
 rho1 = 1.
-G    = 1. #6.673e-11
+G    = 1.
 sig  = 1.
 sig2 = sig**2.
 W0   = 12.
@@ -50,35 +50,6 @@ def dens():
     np.savetxt("test_sols.txt", w, fmt="%.9e")
     return rsol
 
-'''
-#if __name__ == '__main__':
-#    q = dens()
-
-# Manual 4th-order Runge-Kutta
-a = 0.
-b = 100.
-N = 1000
-h = (b-a)/N
-
-rpoints  = np.arange(a,b,h)
-x1points = []
-x2points = []
-
-t = np.array([W0*sig2, 0.],float)
-for r in rpoints:
-    x1points.append(t[0])
-    x2points.append(t[1])
-    k1 = h*solvr(t,r)
-    k2 = h*solvr(t+0.5*k1, r+0.5*h)
-    k3 = h*solvr(t+0.5*k2, r+0.5*h)
-    k4 = h*solvr(t+k3, r+h)
-    t += (k1+2*k2+2*k3+k4)/6
-
-plt.ion()
-plt.plot(rpoints,x1points)
-plt.plot(rpoints,x2points)
-'''
-
 
 def psi_r(r, psi_vals):
     '''
@@ -94,38 +65,56 @@ def psi_r(r, psi_vals):
     val = interpolate.interp1d(r, psi_vals, kind='cubic')
     return val
 
-'''
-def rho_r(r, psi_vals):
-    x1 = psi_r(r, psi_vals)
-    val = rho1 * ((np.exp(x1/sig2) * special.erf(np.sqrt(x1)/sig)) - (np.sqrt((4. * x1)/(np.pi*sig2)) * (1. + ((2*x1)/(3*sig2)))))
-    return val
-'''
 
 q        = dens()
 psi_vals = q[:,0][~np.isnan(q[:,0])]
-rvals = rrange[:len(psi_vals)]
-#rho_vals = rho_r(rvals, psi_vals)
+rvals    = rrange[:len(psi_vals)]
 
 
 def rho_r(r):
-    x1 = psi_vals
+    x1  = psi_vals
     val = rho1 * ((np.exp(x1/sig2) * special.erf(np.sqrt(x1)/sig)) - (np.sqrt((4. * x1)/(np.pi*sig2)) * (1. + ((2*x1)/(3*sig2)))))
     return val
 
 
 rho_vals = rho_r(rvals)
+rho0     = rho_vals[0]
+r0       = np.sqrt( (9. * sig2)/(4. * np.pi * G * rho0) )
+
+print
+print "rho0 is:", rho0
+print "r0 is:", r0
+print
 
 plt.ion()
-plt.loglog(rvals,rho_vals, color = 'purple', lw = 3., label= "$W_0 = 12.0$ ")
+plt.loglog(rvals/r0,rho_vals/rho0, color = 'purple', lw = 3., label= "$W_0 = 12.0$ ")
 plt.xlabel("$\mathrm{log}(R)$",fontsize=20)
 plt.ylabel(r"$\mathrm{log(\rho}(R))$",fontsize=20)
 plt.title("$W_0 = {0}$".format(W0), fontsize=20)
 
 
+
+
 '''
-    data = np.loadtxt("test_sols.txt")
-    plt.ion()
-    plt.plot(data[:,0], data[:,1], 'o')
-    plt.xlabel("$r$", fontsize=20)
-    plt.ylabel("$\psi(r)$", fontsize=20)
-    '''
+    # Manual 4th-order Runge-Kutta
+    a = 0.
+    b = 100.
+    N = 1000
+    h = (b-a)/N
+    
+    rpoints  = np.arange(a,b,h)
+    x1points = []
+    x2points = []
+    
+    t = np.array([W0*sig2, 0.],float)
+    for r in rpoints:
+        x1points.append(t[0])
+        x2points.append(t[1])
+        k1 = h*solvr(t,r)
+        k2 = h*solvr(t+0.5*k1, r+0.5*h)
+        k3 = h*solvr(t+0.5*k2, r+0.5*h)
+        k4 = h*solvr(t+k3, r+h)
+        t += (k1+2*k2+2*k3+k4)/6
+'''
+
+
