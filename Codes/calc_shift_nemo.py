e03ffbd52bb123ff77cc303e8d454e45865ac29f
“Anita <bahmanyar@astro.utoronto.ca>
2016-02-07 14:45:01 -0500

    time is in Gyr and velocity in kpc/Gyr for MW2014 to work but still wron initial condition is returned

--- Codes/calc_shift_nemo.py
+++ Codes/calc_shift_nemo.py
@@ -3,6 +3,7 @@ import mw_transform    as     mw
 import scipy           as     sp
 from   galpy.util      import bovy_conversion
 from   galpy.potential import MWPotential2014
+#from   galpy.potential   import MWPotential2014, PowerSphericalPotentialwCutoff, MiyamotoNagaiPotential, NFWPotential
 
 
 q  = 0.9
@@ -33,19 +34,16 @@ def calc_init_pos(duration, pot_type, Vo, Ro, q=None):
 
     ro = Ro
     vo = Vo
-    ts   = 1000 # number of timesteps
+    ts   = 1001 # number of timesteps
     # we need to convert to galpy time units since its not in Gyrs
-    time = np.linspace(0., nemotime_to_actualtime(duration)/bovy_conversion.time_in_Gyr(vo,ro), ts)
+    #time = np.linspace(0., nemotime_to_actualtime(duration)/bovy_conversion.time_in_Gyr(vo,ro), ts)
+    time = np.linspace(0., duration/bovy_conversion.time_in_Gyr(vo,ro), ts)
     
     if pot_type == "Log":
-        p    = potential.LogarithmicHaloPotential(q = q, normalize = 1)
+        p = potential.LogarithmicHaloPotential(q = q, normalize = 1)
     
     elif pot_type == "MW2014":
-        from   galpy.potential   import MWPotential2014, PowerSphericalPotentialwCutoff, MiyamotoNagaiPotential, NFWPotential
-        bp  = PowerSphericalPotentialwCutoff(alpha=1.8,rc=1.9/8.,normalize=0.05)
-        mp  = MiyamotoNagaiPotential(a=3./8.,b=0.28/8.,normalize=.6)
-        npp = NFWPotential(a=16/8.,normalize=.35)
-        p   = MWPotential2014 = [bp,mp,npp]
+        p = MWPotential2014
 
     # the position and velocity of GD1 stream today in cartesian coordinates
     xnow, ynow, znow    = np.array([12.4,1.5,7.1])
@@ -59,13 +57,22 @@ def calc_init_pos(duration, pot_type, Vo, Ro, q=None):
     o = Orbit(vxvv=[Ri/ro, vri/vo, vti/vo, zcyli/ro, vzcyli/vo, phii], ro=ro, vo=vo)
     o.integrate(time,p)
 
-    x_init = o.x(time[-1], ro = ro, obs= [ro,0.,0.])
-    y_init = o.y(time[-1], ro = ro, obs= [ro,0.,0.])
-    z_init = o.z(time[-1], ro = ro, obs= [ro,0.,0.])
-    
-    vx_init = -o.vx(time[ts-1],ro = ro,obs= [ro,0.,0.])
-    vy_init = -o.vy(time[ts-1],ro = ro,obs= [ro,0.,0.])
-    vz_init = -o.vz(time[ts-1],ro = ro,obs= [ro,0.,0.])
+    #x_init = o.x(time[-1], ro = ro, obs= [ro,0.,0.])[0]
+    #y_init = o.y(time[-1], ro = ro, obs= [ro,0.,0.])[0]
+    #z_init = o.z(time[-1], ro = ro, obs= [ro,0.,0.])
+    #vx_init = -o.vx(time[ts-1],ro = ro,obs= [ro,0.,0.], use_physical=False)[0]*bovy_conversion.velocity_in_kpcGyr(vo,ro)
+    #vy_init = -o.vy(time[ts-1],ro = ro,obs= [ro,0.,0.], use_physical=False)[0]*bovy_conversion.velocity_in_kpcGyr(vo,ro)
+    #vz_init = -o.vz(time[ts-1],ro = ro,obs= [ro,0.,0.], use_physical=False)*bovy_conversion.velocity_in_kpcGyr(vo,ro)
+
+
+    x_init = o.x(time[-1])[0]
+    y_init = o.y(time[-1])[0]
+    z_init = o.z(time[-1])
+
+    vx_init = -o.vx(time[-1],use_physical=False)[0]*bovy_conversion.velocity_in_kpcGyr(vo,ro)
+    vy_init = -o.vy(time[-1],use_physical=False)[0]*bovy_conversion.velocity_in_kpcGyr(vo,ro)
+    vz_init = -o.vz(time[-1],use_physical=False)*bovy_conversion.velocity_in_kpcGyr(vo,ro)
+
 
     return np.array([x_init, y_init, z_init, vx_init, vy_init, vz_init])
 
